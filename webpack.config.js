d682b809f0818e4cad86df21a5d2f582ef2b8c19
F1ks3r <f1ks3r@yandex.ru>
2020-12-23 15:27:32 +0300

    asd

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const TerserPlugin = require('terser-webpack-plugin')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+const argv = require('yargs').argv
+
+
+module.exports = {
+    entry: [
+        './client/index.js'
+    ],
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js'
+    },
+    optimization: {
+        minimize: argv.mode == 'production',
+        minimizer: [
+            new TerserPlugin({
+                parallel: false,
+                extractComments: false,
+                terserOptions: {
+                    compress: {
+                        drop_console: true,
+                    },
+                }
+            })
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js(x?)$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"]
+                }
+            },
+            {
+                test: /\.(scss)$/,
+                exclude: [/node_modules/],
+                use: [
+                    { loader: 'vue-style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' },
+                ]
+            },
+            {
+                test: /\.(sass)$/,
+                use: [
+                    { loader: 'vue-style-loader' },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            indentedSyntax: true,
+                            sourceMap: true
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.(styl|stylus)$/,
+                use: [
+                    { loader: 'vue-style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'stylus-loader' }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.css',
+            '.sass'
+        ]
+    }
+}

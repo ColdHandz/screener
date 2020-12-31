98be074975a85b4dab664546b4980639fb7e11d5
F1ks3r <f1ks3r@yandex.ru>
2020-12-31 17:04:55 +0300

    save

--- client/store.js
+++ client/store.js
@@ -1,9 +1,32 @@
 import { createStore } from 'redux'
 
+// actions
+const increment = () => {
+    return {
+        type: 'increment'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'decrement'
+    }
+}
+
+// reducer
+const counter = (state = 0, action) => {
+    switch(action.type) {
+        case 'increment':
+            return state + 1
+        case 'decrement':
+            return state - 1
+    }
+}
+
 // store
-let store = createStore({})
+let store = createStore(counter)
 
-// action
-
+store.dispatch(increment())
+store.dispatch(decrement())
 
 export default store

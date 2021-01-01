862ccd3515fe098c63dc1ba54a70a5e4b21a2fc2
F1ks3r <f1ks3r@yandex.ru>
2021-01-01 17:23:45 +0300

    save

--- client/index.js
+++ client/index.js
@@ -1,28 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'mobx-react'
 import { Router, Switch, Route } from "react-router-dom"
-import createBrowserHistory from 'history/createBrowserHistory'
-import { RouterStore, syncHistoryWithStore } from 'mobx-react-router'
 
-import store from './store.js'
 import Index from './components/Index.jsx'
 import Company from './components/Company.jsx'
+import store from './store.js'
 import './styles/index.sass'
 
-const browserHistory = createBrowserHistory()
-const routing = new RouterStore()
-const history = syncHistoryWithStore(browserHistory, routing)
-
-
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <Switch>
-                <Route path="/company" children={ <Company /> } />
-                <Route path="/" children={ <Index /> } />
-            </Switch>
-        </Router>
-    </Provider>,
+    <Router history={history}>
+        <Switch>
+            <Route path="/company" children={ <Company /> } />
+            <Route path="/" children={ <Index /> } />
+        </Switch>
+    </Router>,
     document.getElementById('root')
 )

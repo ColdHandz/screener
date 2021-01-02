66a70259af4037f12a2a5318916435d3b294d95c
F1ks3r <f1ks3r@yandex.ru>
2021-01-03 01:37:24 +0300

    save

--- client/components/Index.jsx
+++ client/components/Index.jsx
@@ -8,12 +8,13 @@ import {
     TableHead,
     TableRow,
     IconButton,
+    Collapse,
     Paper
 } from '@material-ui/core'
 
-import { KeyboardArrowDownIcon, KeyboardArrowUpIcon } from '@material-ui/icons'
-// import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
-// import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
+// import { KeyboardArrowDownIcon, KeyboardArrowUpIcon } from '@material-ui/icons'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
 
 @inject('store')
 @observer
@@ -39,24 +40,42 @@ export default class Index extends React.Component {
     }
     render() {
         return (
-            <div>
-                <TableContainer component={Paper}>
-                    <Table size="small" aria-label="a dense table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Тикер</TableCell>
-                                <TableCell onClick={this.sortRowsByVolume} style={{cursor: 'pointer'}}>VOLTODAY1</TableCell>
-                                <TableCell>VOLTODAY2</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {this.state.rows.map((e,i) =>
-                                <Row row={e} key={i} />
-                            )}
-                        </TableBody>
-                    </Table>
-                    </TableContainer>
-            </div>
+            <TableContainer component={Paper}>
+                <Table size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell>SECID</TableCell>
+                            <TableCell>BOARDID</TableCell>
+                            <TableCell>TRADINGSESSION</TableCell>
+                            <TableCell>TIME</TableCell>
+                            <TableCell>PRICEMINUS...</TableCell>
+                            <TableCell>VOLTODAY</TableCell>
+                            <TableCell>VALTODAY</TableCell>
+                            <TableCell>HIGHBID</TableCell>
+                            <TableCell>LOWOFFER</TableCell>
+                            <TableCell>LASTOFFER</TableCell>
+                            <TableCell>LASTBID</TableCell>
+                            <TableCell>OPEN</TableCell>
+                            <TableCell>LOW</TableCell>
+                            <TableCell>HIGH</TableCell>
+                            <TableCell>LAST</TableCell>
+                            <TableCell>LCLOSEPRICE</TableCell>
+                            <TableCell>NUMTRADES</TableCell>
+                            <TableCell>WAPRICE</TableCell>
+                            <TableCell>ADMITTEDQUOTE</TableCell>
+                            <TableCell>MARKETPRICE2</TableCell>
+                            <TableCell>LCURRENTPRICE</TableCell>
+                            <TableCell>CLOSINGAUCTIONPRICE</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.rows.map((e,i) =>
+                            <Row row={e} key={i} />
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         )
     }
 }
@@ -70,10 +89,10 @@ function Row(props) {
             <TableRow>
                 <TableCell>
                     <IconButton aria-label="expand row" size="small" onClick={() => collapseRow(!isRowCollapsed)}>
-                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                        {isRowCollapsed ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
                     </IconButton>
                 </TableCell>
-                <TableCell component="th" scope="row">{row[0]}</TableCell>
+                <TableCell align="left">{row[0]}</TableCell>
                 <TableCell align="right">{row[1]}</TableCell>
                 <TableCell align="right">{row[2]}</TableCell>
                 <TableCell align="right">{row[3]}</TableCell>
@@ -84,9 +103,20 @@ function Row(props) {
                 <TableCell align="right">{row[8]}</TableCell>
                 <TableCell align="right">{row[9]}</TableCell>
                 <TableCell align="right">{row[10]}</TableCell>
+                <TableCell align="right">{row[11]}</TableCell>
+                <TableCell align="right">{row[12]}</TableCell>
+                <TableCell align="right">{row[13]}</TableCell>
+                <TableCell align="right">{row[14]}</TableCell>
+                <TableCell align="right">{row[15]}</TableCell>
+                <TableCell align="right">{row[16]}</TableCell>
+                <TableCell align="right">{row[17]}</TableCell>
+                <TableCell align="right">{row[18]}</TableCell>
+                <TableCell align="right">{row[19]}</TableCell>
+                <TableCell align="right">{row[20]}</TableCell>
+                <TableCell align="right">{row[21]}</TableCell>
             </TableRow>
             <TableRow>
-            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={11}>
+            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={23}>
                 <Collapse in={isRowCollapsed} timeout="auto" unmountOnExit>
                     <div>asd</div>
                 </Collapse>
